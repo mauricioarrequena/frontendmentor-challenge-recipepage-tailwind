c8c6386303b72b6b8f6d4e37856657c08be9d60c
mauricioarrequena <mauricio.ar.requena@gmail..com>
2024-05-23 12:39:48 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia',],
+      'mono': ['ui-monospace', 'SFMono-Regular',],
+      'display': ['Oswald',],
+      'body': ['"Open Sans"',],
+      'young-serif':['"Young Serif"', "serif"],
+      'outfit':['"Outfit"', "sans-serif"],
+            // font-family: "Young Serif", serif;
+      // font-family: "Outfit", sans-serif;
+    }
+  }
+}
